e79db1768a599f847afad0a67d103c6077265ed7
EmilGsson <emilgoransson1@hotmail.com>
2025-09-30 21:21:33 +0200

    Text now prints company, last update and offline-warning

--- report-maker.py
+++ report-maker.py
@@ -9,16 +9,24 @@ data = json.load(open("network_devices.json", "r", encoding = "utf-8"))
 # create a variable that holds our whole text report
 
 report = ""
+
+## print company name and last_updated from json
+report += data["company"] + " | " + data["last_updated"] + "\n\n"
+
 #loop location list
 #location lista av "locations" i json-fil
-
 for location in data["locations"]:
     #print the site
     report += location ["site"] +"\n"
-    
+   
         #print hostname devicers on locations
     for device in location ["devices"]:
         report += "  " + device["hostname"] + "\n"
+        # IF status != (is not) online, print status
+        if device["status"] != "online":
+            report +=  device["hostname"] + " | " + device ["status"] + "\n"
+
+        
 
 ##write the report to text file
 
