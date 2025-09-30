e680dcd4d11144bc9591307db487d9f4060dbb93
EmilGsson <emilgoransson1@hotmail.com>
2025-09-30 22:54:27 +0200

    list now prints uptime less 30 days and counts devices

--- report-maker.py
+++ report-maker.py
@@ -10,8 +10,15 @@ data = json.load(open("network_devices.json", "r", encoding = "utf-8"))
 
 report = ""
 
+#count variabel for counting types
+
+count = 0
+
 ## print company name and last_updated from json
 report += data["company"] + " | " + data["last_updated"] + "\n\n"
+
+#variabele for counting units
+count = {}
 
 #loop location list
 #location lista av "locations" i json-fil
@@ -25,9 +32,36 @@ for location in data["locations"]:
         # IF status != (is not) online, print status
         if device["status"] != "online":
             report +=  device["hostname"] + " | " + device ["status"] + "\n"
+            
 
+#variabele for counting units.Tried = 0 and it does not allow to find (what i cound find)
+count = {}
+
+##loop list to find all variables in type and count them with "get" https://www.w3schools.com/python/ref_dictionary_get.asp
+
+for location in data["locations"]:
+    for device in location["devices"]:
+        x = device.get("type")
+        ## this code under printed all types in the terminal showing my loop worked. just for fun
+        ###print (x) 
+        count [x] = count.get(x, 0) +1
+
+#writes this text to make document look better       
+report += "Type of devices:\n"
         
+## for "x" in count (variable i created) write x + count in the text file. 
+for x in count:                       
+    report += x +  ": " + str(count[x]) + "\n"
+            
+#for text format            
+report += "\nDevices with less than 30 days uptime:\n"
 
+for location in data["locations"]:
+    for device in location["devices"]:
+        if device["uptime_days"] < 30:
+            report += device["hostname"] + " | " + str(device["uptime_days"]) + " days\n"
+        
+        
 ##write the report to text file
 
 with open('report.txt', 'w', encoding='utf-8') as f:
