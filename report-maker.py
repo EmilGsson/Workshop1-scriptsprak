f9e0cb74af708779a5945e4987aa7cab463c45f3
EmilGsson <emilgoransson1@hotmail.com>
2025-09-30 19:49:52 +0200

    printing site locations on each location

--- report-maker.py
+++ report-maker.py
@@ -1,1 +1,18 @@
-print("hej")
+# Import the json library so that we can handle json
+import json
+
+
+
+# Read json from products.json to the variable products
+data = json.load(open("network_devices.json", "r", encoding = "utf-8"))
+
+
+#loop location list
+#location lista av "locations" i json-fil
+
+for location in data["locations"]:
+    #print the site
+    print (location ["site"])
+    #print hostname devicers on locations
+    for device in location ["devices"]:
+        print(" ", device["hostname"])
